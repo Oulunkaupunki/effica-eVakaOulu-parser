1aff21423b4f5171747f630d7367387b6cb4f517
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2021-12-30 14:45:32 +0200

    Added just.config.integration-test.js

---
+++ jest.config.integration-test.js
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2021 City of Oulu
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["**/integration-test/**/*.[jt]s?(x)"],
+};
